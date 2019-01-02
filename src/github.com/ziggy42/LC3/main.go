614aea9fb6f1e29a06f8339866ed4d4210a0984e
AndreaPivetta <vanpivix@gmail.com>
2019-01-02 11:58:09 +0100

    Rename memory and registers

--- src/github.com/ziggy42/LC3/main.go
+++ src/github.com/ziggy42/LC3/main.go
@@ -69,11 +69,11 @@ const (
 // PC_START sets the PC starting position. 0x3000 is the default position
 const PC_START = 0x3000
 
-var MEMORY [^uint16(0)]uint16
-var REGISTERS [R_COUNT]uint16
+var memory [^uint16(0)]uint16
+var registers [R_COUNT]uint16
 
 func memWrite(address uint16, value uint16) {
-	MEMORY[address] = value
+	memory[address] = value
 }
 
 func keyPressed() bool {
@@ -84,19 +84,19 @@ func keyPressed() bool {
 func memRead(address uint16) uint16 {
 	if address == MR_KBSR {
 		if keyPressed() {
-			MEMORY[MR_KBSR] = (1 << 15)
+			memory[MR_KBSR] = (1 << 15)
 			c, err := getChar()
 			if err != nil {
 				panic(err)
 			}
 
-			MEMORY[MR_KBDR] = c
+			memory[MR_KBDR] = c
 		} else {
-			MEMORY[MR_KBSR] = 0
+			memory[MR_KBSR] = 0
 		}
 	}
 
-	return MEMORY[address]
+	return memory[address]
 }
 
 func signExtend(x uint16, bitCount uint) uint16 {
@@ -108,12 +108,12 @@ func signExtend(x uint16, bitCount uint) uint16 {
 }
 
 func updateFlags(r uint16) {
-	if REGISTERS[r] == 0 {
-		REGISTERS[R_COND] = FL_ZRO
-	} else if REGISTERS[r]<<15 == 1 { // Right-most bit is 1 for negative numbers
-		REGISTERS[R_COND] = FL_NEG
+	if registers[r] == 0 {
+		registers[R_COND] = FL_ZRO
+	} else if registers[r]<<15 == 1 { // Right-most bit is 1 for negative numbers
+		registers[R_COND] = FL_NEG
 	} else {
-		REGISTERS[R_COND] = FL_POS
+		registers[R_COND] = FL_POS
 	}
 }
 
@@ -133,23 +133,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	err := Load(&MEMORY, args[0])
+	err := Load(&memory, args[0])
 	if err != nil {
 		panic(err)
 	}
 
-	REGISTERS[R_PC] = PC_START
+	registers[R_PC] = PC_START
 	running := true
 	for running {
-		instruction := MEMORY[REGISTERS[R_PC]]
-		REGISTERS[R_PC]++
+		instruction := memory[registers[R_PC]]
+		registers[R_PC]++
 
 		switch op := instruction >> 12; op {
 		case OP_BR:
 			pcOffset := signExtend((instruction)&0x1ff, 9)
 			flag := (instruction >> 9) & 0x7
-			if flag&REGISTERS[R_COND] != 0 {
-				REGISTERS[R_PC] += pcOffset
+			if flag&registers[R_COND] != 0 {
+				registers[R_PC] += pcOffset
 			}
 		case OP_ADD:
 			dr := (instruction >> 9) & 0x7
@@ -158,10 +158,10 @@ func main() {
 
 			if mode == MODE_IMMEDIATE {
 				immediate := signExtend(instruction&0x1F, 5)
-				REGISTERS[dr] = REGISTERS[sr1] + immediate
+				registers[dr] = registers[sr1] + immediate
 			} else {
 				sr2 := instruction & 0x7
-				REGISTERS[dr] = REGISTERS[sr1] + REGISTERS[sr2]
+				registers[dr] = registers[sr1] + registers[sr2]
 			}
 
 			updateFlags(dr)
@@ -169,22 +169,22 @@ func main() {
 			dr := (instruction >> 9) & 0x7
 			pcOffset := signExtend(instruction&0x1ff, 9)
 
-			REGISTERS[dr] = memRead(REGISTERS[R_PC] + pcOffset)
+			registers[dr] = memRead(registers[R_PC] + pcOffset)
 			updateFlags(dr)
 		case OP_ST:
 			sr := (instruction >> 9) & 0x7
 			pcOffset := signExtend(instruction&0x1ff, 9)
-			memWrite(REGISTERS[R_PC]+pcOffset, REGISTERS[sr])
+			memWrite(registers[R_PC]+pcOffset, registers[sr])
 		case OP_JSR:
 			r := (instruction >> 6) & 0x7
 			longPcOffset := signExtend(instruction&0x7ff, 11)
 			longFlag := (instruction >> 11) & 1
 
-			REGISTERS[R_R7] = REGISTERS[R_PC]
+			registers[R_R7] = registers[R_PC]
 			if longFlag != 0 {
-				REGISTERS[R_PC] += longPcOffset
+				registers[R_PC] += longPcOffset
 			} else {
-				REGISTERS[R_PC] = REGISTERS[r]
+				registers[R_PC] = registers[r]
 			}
 		case OP_AND:
 			dr := (instruction >> 9) & 0x7
@@ -193,10 +193,10 @@ func main() {
 
 			if mode == MODE_IMMEDIATE {
 				imm5 := signExtend(instruction&0x1F, 5)
-				REGISTERS[dr] = REGISTERS[sr1] & imm5
+				registers[dr] = registers[sr1] & imm5
 			} else {
 				sr2 := instruction & 0x7
-				REGISTERS[dr] = REGISTERS[sr1] & REGISTERS[sr2]
+				registers[dr] = registers[sr1] & registers[sr2]
 			}
 
 			updateFlags(dr)
@@ -204,42 +204,42 @@ func main() {
 			dr := (instruction >> 9) & 0x7
 			sr1 := (instruction >> 6) & 0x7
 			offset := signExtend(instruction&0x3F, 6)
-			REGISTERS[dr] = memRead(REGISTERS[sr1] + offset)
+			registers[dr] = memRead(registers[sr1] + offset)
 
 			updateFlags(dr)
 		case OP_STR:
 			sr1 := (instruction >> 9) & 0x7
 			sr2 := (instruction >> 6) & 0x7
 			offset := signExtend(instruction&0x3F, 6)
-			memWrite(REGISTERS[sr2]+offset, REGISTERS[sr1])
+			memWrite(registers[sr2]+offset, registers[sr1])
 		case OP_RTI:
 			panic("Unknown opcode")
 		case OP_NOT:
 			dr := (instruction >> 9) & 0x7
 			sr1 := (instruction >> 6) & 0x7
 
-			REGISTERS[dr] = ^REGISTERS[sr1]
+			registers[dr] = ^registers[sr1]
 			updateFlags(dr)
 		case OP_LDI:
 			dr := (instruction >> 9) & 0x7
 			pcOffset := signExtend(instruction&0x1ff, 9)
 
-			REGISTERS[dr] = memRead(memRead(REGISTERS[R_PC] + pcOffset))
+			registers[dr] = memRead(memRead(registers[R_PC] + pcOffset))
 			updateFlags(dr)
 		case OP_STI:
 			sr1 := (instruction >> 9) & 0x7
 			pcOffset := signExtend(instruction&0x1ff, 9)
-			memWrite(memRead(REGISTERS[R_PC]+pcOffset), REGISTERS[sr1])
+			memWrite(memRead(registers[R_PC]+pcOffset), registers[sr1])
 		case OP_JMP:
 			sr1 := (instruction >> 6) & 0x7
-			REGISTERS[R_PC] = REGISTERS[sr1]
+			registers[R_PC] = registers[sr1]
 		case OP_RES:
 			panic("Unknown opcode")
 		case OP_LEA:
 			dr := (instruction >> 9) & 0x7
 			pcOffset := signExtend(instruction&0x1ff, 9)
 
-			REGISTERS[dr] = REGISTERS[R_PC] + pcOffset
+			registers[dr] = registers[R_PC] + pcOffset
 			updateFlags(dr)
 		case OP_TRAP:
 			switch instruction & 0xFF {
@@ -248,13 +248,13 @@ func main() {
 				if err != nil {
 					panic(err)
 				}
-				REGISTERS[R_R0] = c
+				registers[R_R0] = c
 			case TRAP_OUT:
-				fmt.Printf("%c", rune(REGISTERS[R_R0]))
+				fmt.Printf("%c", rune(registers[R_R0]))
 			case TRAP_PUTS:
-				i := REGISTERS[R_R0]
-				for ; MEMORY[i] != 0; i++ {
-					fmt.Printf("%c", rune(MEMORY[i]))
+				i := registers[R_R0]
+				for ; memory[i] != 0; i++ {
+					fmt.Printf("%c", rune(memory[i]))
 				}
 			case TRAP_IN:
 				fmt.Print("Enter a character: ")
@@ -262,13 +262,13 @@ func main() {
 				if err != nil {
 					panic(err)
 				}
-				REGISTERS[R_R0] = c
+				registers[R_R0] = c
 			case TRAP_PUTSP:
-				i := REGISTERS[R_R0]
-				for ; MEMORY[i] != 0; i++ {
-					r1 := rune(MEMORY[i] & 0xFF)
+				i := registers[R_R0]
+				for ; memory[i] != 0; i++ {
+					r1 := rune(memory[i] & 0xFF)
 					fmt.Printf("%c", r1)
-					r2 := rune(MEMORY[i] >> 8)
+					r2 := rune(memory[i] >> 8)
 					if r2 != 0 {
 						fmt.Printf("%c", r2)
 					}
